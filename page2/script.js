44f7349c270c528606cc3db248f62d18aff1165d
unknown <kiritouchia999@gmail.com>
2022-10-09 16:02:30 +0700

    update new

--- page2/script.js
+++ page2/script.js
@@ -1,7 +1,77 @@
-function attackFunc() {
-  alert("Basic attack");
+var attackType;
+var sign_arr = ["paper", "rock", "scissors"];
+
+document.getElementById("choose").style.display = "none";
+
+function showType(status) {
+  document.getElementById("choose").style.display = "block";
+  attackType = status;
+  return;
 }
 
-function ultiFunc() {
-  alert("Ultimate attack");
+function attackFunc(sign) {
+  if (parseInt(attackType) === 0) {
+    const damage = 100;
+    const result = sign_arr[Math.floor(Math.random() * sign_arr.length)];
+    exeDamage(sign, damage, result);
+  } else if (parseInt(attackType) === 1) {
+    const damage = 1000;
+    const result = sign_arr[Math.floor(Math.random() * sign_arr.length)];
+    exeDamage(sign, damage, result);
+  } else {
+  }
+}
+
+function exeDamage(sign, damage, result) {
+  const human_health = document.getElementById("human_health").innerHTML;
+  const monster_health = document.getElementById("monster_health").innerHTML;
+  var jumlah;
+  if (sign === "paper") {
+    if (result === "paper") {
+      //   alert("Draw");
+      //   alert(result);
+    } else if (result === "rock") {
+      jumlah = parseInt(monster_health) - parseInt(damage);
+      document.getElementById("monster_health").innerHTML = jumlah;
+      //   alert("Win");
+      //   alert(result);
+    } else if (result === "scissors") {
+      //   alert("Lose");
+      //   alert(result);
+      jumlah = parseInt(human_health) - parseInt(damage);
+      document.getElementById("human_health").innerHTML = jumlah;
+    }
+  } else if (sign === "rock") {
+    if (result === "paper") {
+      //   alert("Lose");
+      //   alert(result);
+      jumlah = parseInt(human_health) - parseInt(damage);
+      document.getElementById("human_health").innerHTML = jumlah;
+    } else if (result === "rock") {
+      //   alert("Draw");
+      //   alert(result);
+    } else if (result === "scissors") {
+      //   alert("Win");
+      //   alert(result);
+      jumlah = parseInt(monster_health) - parseInt(damage);
+      document.getElementById("monster_health").innerHTML = jumlah;
+    }
+  } else if (sign === "scissors") {
+    if (result === "paper") {
+      //   alert("Win");
+      //   alert(result);
+      jumlah = parseInt(monster_health) - parseInt(damage);
+      document.getElementById("monster_health").innerHTML = jumlah;
+    } else if (result === "rock") {
+      //   alert("Lose");
+      //   alert(result);
+      jumlah = parseInt(human_health) - parseInt(damage);
+      document.getElementById("human_health").innerHTML = jumlah;
+    } else if (result === "scissors") {
+      //   alert("Draw");
+      //   alert(result);
+    }
+  }
+
+  return;
 }
